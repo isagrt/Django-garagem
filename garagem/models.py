9c9be78527f7c8c054474fd362426a3ccf98d96b
isagrt <isadoragoedert05@gmail.com>
2023-03-13 14:44:21 -0300

    commit criação do modelo categoria

--- garagem/models.py
+++ garagem/models.py
@@ -4,5 +4,11 @@ class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, null=True, blank=True)
 
-def __str__(self):
-        return self.nome.upper
+    def __str__(self):
+        return self.nome.upper()
+    
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
