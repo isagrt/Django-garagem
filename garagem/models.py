afc39b41c659670bc6d10a6c115afa9d4a031fd1
isagrt <isadoragoedert05@gmail.com>
2023-03-28 16:38:03 -0300

    criação do modelo veiculo

--- garagem/models.py
+++ garagem/models.py
@@ -25,3 +25,8 @@ class Cor(models.Model):
     def __str__(self):
         return self.descricao
     
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    ano = models.IntegerField(max_length=int,  )
