ae0f793ed487d82beab2a978e3d29bddefa52a62
isagrt <isadoragoedert05@gmail.com>
2023-03-28 16:54:36 -0300

    alteração do modelo veiculo

--- garagem/models.py
+++ garagem/models.py
@@ -29,4 +29,8 @@ class Veiculo(models.Model):
     marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
-    ano = models.IntegerField(max_length=int,  )
+    ano = models.IntegerField( null=True, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.marca} {self.categoria} {self.ano} {self.cor}" 
