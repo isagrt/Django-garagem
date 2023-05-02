064a8675e529cc3e7eb30a3a980600a4e31cc6aa
isagrt <isadoragoedert05@gmail.com>
2023-05-01 21:34:05 -0300

    atualização aula 8.5

--- garagem/views.py
+++ garagem/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from garagem.models import Marca, Categoria, Cor, Veiculo, Acessorio
+from garagem.serializers import (
+    MarcaSerializer,
+    CategoriaSerializer,
+    CorSerializer,
+    VeiculoSerializer,
+    VeiculoListSerializer,
+    VeiculoDetailSerializer,
+    AcessorioSerializer,
+)
+
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer
+
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_classes = {
+        "list": VeiculoListSerializer,
+        "retrieve": VeiculoDetailSerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, VeiculoSerializer)
--- serializers.py
+++ serializers.py
@@ -1,8 +1,42 @@
 from rest_framework.serializers import ModelSerializer
 
-from garagem.models import Marca
+from garagem.models import Marca, Categoria, Cor, Veiculo, Acessorio
+
 
 class MarcaSerializer(ModelSerializer):
     class Meta:
         model = Marca
         fields = "__all__"
+
+class CategoriaSerializer(ModelSerializer):
+    class Meta:
+        model = Categoria
+
+        fields = "__all__"
+class AcessorioSerializer(ModelSerializer):
+    class Meta:
+        model = Acessorio
+        fields = "__all__"
+
+
+class CorSerializer(ModelSerializer):
+    class Meta:
+        model = Cor
+        fields = "__all__"
+        
+class VeiculoSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = "__all__"
+
+class VeiculoDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = "__all__"
+        depht = 1
+
+
+class VeiculoListSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = ["categoria", "marca", "ano"]
