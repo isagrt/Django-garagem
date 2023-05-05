a2eecc94e0ef7e8e9a17e6a45f05e5dc6e085d8f
MarceloHarbs <mharbs614@gmail.com>
2023-05-05 16:34:04 -0300

    correção do código

--- garagem/views.py
+++ garagem/views.py
@@ -1,47 +1,31 @@
 from rest_framework.viewsets import ModelViewSet
 
-from garagem.models import Acessorio, Categoria, Cor, Marca, Modelo, Veiculo
-from garagem.serializers import (
-    AcessorioSerializer,
-    CategoriaSerializer,
-    CorSerializer,
-    MarcaSerializer,
-    ModeloSerializer,
-    VeiculoSerializer,
-    VeiculoDetailSerializer
-)
-
+from garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo
+from garagem.serializers import AcessorioSerializer, CategoriaSerializer, CorSerializer, MarcaSerializer, VeiculoSerializer, VeiculoDetailSerializer, VeiculoListSerializer
 
 class AcessorioViewSet(ModelViewSet):
     queryset = Acessorio.objects.all()
     serializer_class = AcessorioSerializer
 
-
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
-
 
 class CorViewSet(ModelViewSet):
     queryset = Cor.objects.all()
     serializer_class = CorSerializer
 
-
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer
 
-
-class ModeloViewSet(ModelViewSet):
-    queryset = Modelo.objects.all()
-    serializer_class = ModeloSerializer
-
-
 class VeiculoViewSet(ModelViewSet):
     queryset = Veiculo.objects.all()
-    serializer_class = VeiculoSerializer
-    
+    serializer_classes = {
+        "list": VeiculoListSerializer,
+        "retrieve": VeiculoDetailSerializer,
+    }
+
     def get_serializer_class(self):
-        if self.action in ["list", "retrieve"]:
-            return VeiculoDetailSerializer
-        return VeiculoSerializer
+        return self.serializer_classes.get(self.action, VeiculoSerializer)
+    
