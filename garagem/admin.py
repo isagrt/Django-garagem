6f3371c5a81a2000be3782fa561c0873a8a7c05b
isagrt <isadoragoedert05@gmail.com>
2023-08-17 18:04:03 -0300

    criação da pasta views

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-
-from garagem.models import Marca, Categoria, Acessorio, Cor, Veiculo
+from .models import Marca, Categoria, Acessorio, Cor, Veiculo, Modelo
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
 admin.site.register(Acessorio)
 admin.site.register(Cor)
+admin.site.register(Modelo)
 admin.site.register(Veiculo)
