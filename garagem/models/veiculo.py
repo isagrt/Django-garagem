65bc45ee9fa3d458fe844dc4fd745e148c8fdcc1
isagrt <isadoragoedert05@gmail.com>
2023-08-31 13:30:46 -0300

    Criação pastas - models, serializers e views

---
+++ garagem/models/veiculo.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from garagem.models import Cor, Modelo, Acessorio
+
+
+
+class Veiculo(models.Model):
+    descricao = models.CharField(max_length=100, null=True, blank=True ,default="")
+    preco = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0, null=True, blank=True
+    )
+    ano = models.IntegerField(null=True, blank=True)
+    acessorios = models.ManyToManyField(Acessorio, related_name="veiculos")
+    cor = models.ForeignKey(
+        Cor, on_delete=models.PROTECT, related_name="veiculos"
+    )
+    modelo = models.ForeignKey(
+        Modelo,
+        on_delete=models.PROTECT,
+        related_name="veiculos",
+        default=2,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f"{self.modelo} ({self.descricao})"
+
+    class Meta:
+        verbose_name = "Veículo"
---
+++ garagem/serializers/veiculo.py
@@ -0,0 +1,18 @@
+from rest_framework.serializers import ModelSerializer
+from garagem.models import Veiculo
+
+class VeiculoSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = "__all__"
+
+class VeiculoDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = "__all__"
+        depth = 1
+
+class VeiculoListSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = ["categoria", "marca", "ano", "cor"]
